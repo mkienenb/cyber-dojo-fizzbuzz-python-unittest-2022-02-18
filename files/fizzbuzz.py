02fc231fe92ec5cfe440bb8f63185bc645f404c1
5VtUFB <5VtUFB@cyber-dojo.org>
2022-02-18 16:31:21 +0000

    13 ran tests, predicted green, got green

--- files/fizzbuzz.py
+++ files/fizzbuzz.py
@@ -4,6 +4,6 @@ class Fizzbuzz:
         result = str(number)
         if (number % 3) == 0:
             result = "Fizz"
-        if number == 5:
+        if (number % 5) == 0:
             result = "Buzz"
         return result
