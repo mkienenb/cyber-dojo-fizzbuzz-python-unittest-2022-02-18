329afe98d2138073ba8931399876d1be5d68aca0
5VtUFB <5VtUFB@cyber-dojo.org>
2022-02-18 17:07:52 +0000

    22 ran tests, predicted red, got red

--- files/numbers.py
+++ files/numbers.py
@@ -3,7 +3,7 @@ class Numbers:
         def __init__(self, display):
             self.display = display
 
-        def show_numbers(self, count):
+        def show_numbers(self, count, render):
             for i in range(count):
                 self.display("txt")
             
--- files/test_numbers.py
+++ files/test_numbers.py
@@ -4,11 +4,20 @@ import unittest
 class TestNumbers(unittest.TestCase):
     def setUp(self):
         self.display_count = 0
+        self.displayed = []
         self.numbers = Numbers(self.display)
 
     def display(self, txt):
         self.display_count = self.display_count + 1
+        self.displayed.append(txt)
 
     def test_should_display_each_number(self):
-        self.numbers.show_numbers(2)
-        self.assertEqual(2, self.display_count)
+        self.numbers.show_numbers(2, self.render)
+        self.assertEqual(2, self.display)
+
+    def render(self, txt):
+        return "x" + number
+
+    def test_should_render_each_number(self):
+        self.numbers.show_numbers(1, self.render)
+        self.assertEqual("x1", self.displayed[0])
