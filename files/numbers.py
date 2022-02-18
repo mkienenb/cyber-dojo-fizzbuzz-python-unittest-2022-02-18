57ebb92d224f2dde2ce4bd8a2967d42aac1bd99e
5VtUFB <5VtUFB@cyber-dojo.org>
2022-02-18 16:50:58 +0000

    16 ran tests, predicted red, got amber

---
+++ files/numbers.py
@@ -0,0 +1,10 @@
+from fizzbuzz import Fizzbuzz
+
+class Numbers:
+
+        def __init__(self, display):
+            self.display = display
+
+        def show_numbers(self, count):
+            pass
+            
---
+++ files/test_numbers.py
@@ -0,0 +1,17 @@
+from numbers import Numbers
+import unittest
+
+class TestNumbers(unittest.TestCase):
+    def setup(self):
+        self.display_count + 0
+        self.numbers = Numbers(self.display)
+
+    def display(self, txt):
+        self.display_count = self.display_count + 1
+
+    def test_should_display_each_number(self):
+        self.numbers.show_numbers(2)
+        self.assertEqual(2, self.display_count)
+
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
